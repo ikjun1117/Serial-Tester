91604eed87279de16115887dca4a1f000a046e69
익준 진 <jij1117@naver.com>
2022-07-27 11:00:30 +0900

    초기 커밋.

---
+++ Serial Tester/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.IO.Ports;
+
+namespace SerialTester
+{
+    class MainApp
+    {
+        public static void Main(string[] args)
+        {
+            SerialWorker worker = new SerialWorker();
+            Console.WriteLine("Hex값, 띄어쓰기, 대문자, break입력 시 종료");
+            while (true)
+            {
+                string arg = Console.ReadLine();
+
+                if (arg.Equals("quit"))
+                    break;
+                else
+                {
+                    string[] temp = arg.Split(" ");
+                    byte[] ttemp = new byte[temp.Length];
+                    for (int i = 0; i < temp.Length; i++)
+                    {
+                        ttemp[i] = Convert.ToByte(temp[i], 16);
+                    }
+                    worker.writeData(ttemp);
+                }
+            }
+            worker.closePort();
+        }
+    }
+}
